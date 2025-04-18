600ab0cf7030fa46e9b252cbbbd04a7acf60d5d3
cmd-anurag <anuragkumar.7157@gmail.com>
2025-04-18 20:59:23 +0530

    implement Trie data structure

---
+++ TrieImplementation.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class TrieNode
+{
+public:
+    unordered_map<char, TrieNode *> children;
+    bool isEndOfWord;
+
+    TrieNode()
+    {
+        isEndOfWord = false;
+    }
+};
+
+class Trie
+{
+private:
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    void insert(string &word)
+    {
+        TrieNode *current = root;
+        for (char c : word)
+        {
+            if (current->children.find(c) == current->children.end())
+            {
+                current->children[c] = new TrieNode();
+            }
+            current = current->children[c];
+        }
+        current->isEndOfWord = true;
+    }
+
+    bool search(string &word)
+    {
+        TrieNode *current = root;
+        for (char c : word)
+        {
+            if (current->children.find(c) == current->children.end())
+            {
+                return false;
+            }
+            current = current->children[c];
+        }
+        return current->isEndOfWord;
+    }
+
+    bool isPrefix(string &word)
+    {
+        TrieNode *current = root;
+        for (char c : word)
+        {
+            if (current->children.find(c) == current->children.end())
+            {
+                return false;
+            }
+            current = current->children[c];
+        }
+        return true;
+    }
+};
