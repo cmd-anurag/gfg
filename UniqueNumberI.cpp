00fac5a98a93ec562512593a0730f13d93887ad4
cmd-anurag <anuragkumar.7157@gmail.com>
2025-04-22 17:21:36 +0530

    implement function to identify the unique number in an array

---
+++ UniqueNumberI.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using std::vector;
+
+class Solution
+{
+public:
+    int findUnique(vector<int> &arr)
+    {
+        // code here
+        int res = 0;
+        for (int i : arr)
+        {
+            res ^= i;
+        }
+        return res;
+    }
+};
